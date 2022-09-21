40ba4bd954853ae7902420ed8ec893a05e7d40ad
Hassan Nawazish <50699910+Hassannawazish@users.noreply.github.com>
2022-09-21 19:38:38 +0500

    threadIdx_understand_2d

--- Cuda_base/script.cu
+++ Cuda_base/script.cu
@@ -5,11 +5,22 @@
 #include <iostream>
 
 __global__ void cudd(){                                                       // it is a kernal/device code
-	printf("Hasan Nawazish printing using cuda");
+	printf("Hasan Nawazish printing using cuda \n");
+}
+
+__global__ void print_threadids(){
+	printf("threadIDx.x : %d, threadIDx.y : %d, threadIDx.z : %d \n",
+		threadIdx.x, threadIdx.y, threadIdx.z);
 }
 
 int main(){                                                                   // Host Code
-cudd <<<1,1>>>();
-cudaDeviceSynchronize();                                                      // wait untile previously launched kernals finishes
-cudaDeviceReset();
+	int nx(16), ny(16);
+
+	dim3 block(8,8);
+	dim3 grid(nx / block.x, ny / block.y);
+	print_threadids <<<grid,block>>>();
+
+	cudaDeviceSynchronize();                                                      // wait untile previously launched kernals finishes
+	cudaDeviceReset();
 }
+ 
